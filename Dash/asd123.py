f380bd41d31dd60618bf771ce8fb4868e5f781a4
Manhatai <matixxx.mp4@gmail.com>
2024-02-16 16:58:54 +0100

    Dash

---
+++ Dash/asd123.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+s = int(input("Podaj liczbę: "))
+
+x = "+" * s
+
+
+for i in range(0, s):
+    print(x)
